22f440a0fc61dc4c51822c5d2a4f73bf54ca00c6
MPM <michelpurper@gmail.com>
2013-03-05 21:35:12 -0300

    Trabalho2 Implementação da classe Somatorio

--- entregas/trabalho2/src/main/java/br/com/senacrs/alp/aulas/Somatorio.java
+++ entregas/trabalho2/src/main/java/br/com/senacrs/alp/aulas/Somatorio.java
@@ -1,27 +1,31 @@
 package br.com.senacrs.alp.aulas;
 
-
 public class Somatorio {
-	
+
 	private static Somatorio instancia = new Somatorio();
-	
-	
+
 	private Somatorio() {
 
 		super();
 	}
-	
+
 	public double somaTotal(double[] valores) {
-		
+
 		double resultado = 0.0;
-		
-		//Implementar
-		
+
+		// Implementar
+		if (valores == null)
+			resultado = Double.NaN;
+		else
+			for (double d : valores) {
+				resultado += d;
+			}
+
 		return resultado;
 	}
-	
+
 	public static Somatorio getInstancia() {
-		
+
 		return Somatorio.instancia;
 	}
 }
