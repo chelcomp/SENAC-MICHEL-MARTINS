e90f3b820f489e4d439d567fa75a08699b823b40
MPM <michelpurper@gmail.com>
2013-03-28 19:50:55 -0300

    Ajuste para usar a lista

--- entregas/trabalho5/src/main/java/br/com/senacrs/alp/aulas/FilaClass.java
+++ entregas/trabalho5/src/main/java/br/com/senacrs/alp/aulas/FilaClass.java
@@ -2,59 +2,38 @@ package br.com.senacrs.alp.aulas;
 
 public class FilaClass<T extends Object> implements Fila<T> {
 
-	Nodo<T> nodoInicial = null;
-	Nodo<T> nodoFinal = null;
-	int size;
-
+	Lista<T> lista = ListaFactory.getInstancia().criarLista();
+	
 	@Override
 	public void insercao(T valor) {
-		if (valor == null)
-			throw new IllegalArgumentException(
-					"Não é permitido a inserção de valor NULL na Fila");
-
-		Nodo<T> newNodo = new Nodo<T>(valor);
-
-		if (nodoInicial == null) {
-			nodoInicial = newNodo;
-		} else {
-			nodoFinal.setSubNodo(newNodo);
-
-		}
-		nodoFinal = newNodo;
-		size++;
+		lista.adicionarFinal(valor);
 	}
 
 	@Override
 	public T obter() {
-		if (nodoInicial == null)
-			throw new IllegalArgumentException("A Fila esta vazia");
-
-		return nodoInicial.getValue();
+		return lista.obterPrimeiro();
 	}
 
 	@Override
 	public T remocao() {
 		T r = obter();
-		nodoInicial = nodoInicial.getSubNodo();
-		size--;
+		lista.removerPosicao(0);
 		return r;
 	}
 
 	@Override
 	public boolean vazia() {
-		return nodoInicial == null;
+		return tamanho() == 0;
 	}
 
 	@Override
 	public int tamanho() {
-		return size;
+		return lista.obterTamanho();
 	}
 
 	@Override
 	public void esvaziar() {
-		nodoInicial = null;
-		nodoFinal = null;
-		size = 0;
+		lista.esvaziar();
 	}
 
 }
