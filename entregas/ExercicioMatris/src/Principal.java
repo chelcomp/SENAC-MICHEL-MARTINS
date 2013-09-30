4cda7651a8701b4028a10b5bb5b56718fb353654
chelcomp <michelpurper@gmail.com>
2013-09-29 23:37:24 -0300

    Limpeza de uses

--- entregas/ExercicioMatris/src/Principal.java
+++ entregas/ExercicioMatris/src/Principal.java
@@ -1,8 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Scanner;
 
 public class Principal {
